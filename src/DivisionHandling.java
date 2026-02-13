850a84fc3e8f307c654e94b1dfa8a102e34df700
Jerin Prakash <jerinprakash892@gmail.com>
2026-02-13 09:26:50 +0530

    code added

--- src/DivisionHandling.java
+++ src/DivisionHandling.java
@@ -7,5 +7,26 @@ public class DivisionHandling {
         // TODO: Enclose the division in a try block
         
         // TODO: Catch ArithmeticException
-        
+
+	public static void main(String[] args){
+
+		Scanner sc= new Scanner(System.in);
+
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+
+		try{
+
+			int r=a/b;
+			System.out.println(r);
+
+		}
+		catch(ArithmeticException e){
+
+			System.out.println("Divide by zero error");
+
+		}
+		sc.close();
+
+	}        
 }
